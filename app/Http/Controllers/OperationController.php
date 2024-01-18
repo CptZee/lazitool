55a09ff5de596a754a4a9e009972aee42c93933c
Aaron James R. Mission <68337605+CptZee@users.noreply.github.com>
2024-01-18 11:48:02 +0800

    Added controller functions (Basic CRUD)

--- app/Http/Controllers/OperationController.php
+++ app/Http/Controllers/OperationController.php
@@ -4,8 +4,69 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Models\Operation;
 
 class OperationController extends Controller
 {
-    //
+    public function showAll()
+    {
+        return Operation::all();
+    }
+
+    public function show($id)
+    {
+        return Operation::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'type_id' => 'required|integer|exists:types,id',
+                'file_name' => 'required|string',
+                'file_size' => 'required|integer|min:0'
+            ]);
+    
+            return Operation::create($validatedData);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $model = Operation::findOrFail($id);
+    
+            $validatedData = $request->validate([
+                'type_id' => 'required|integer|exists:types,id',
+                'file_name' => 'required|string',
+                'file_size' => 'required|integer|min:0',
+            ]);
+    
+            $model->update($validatedData);
+    
+            return $model;
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 404);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $model = Operation::findOrFail($id);
+            $model->delete();
+    
+            return response()->json(['message' => 'Record archived successfully'], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Record not found'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -4,8 +4,65 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Models\Type;
 
 class TypeController extends Controller
 {
-    //
+    public function showAll()
+    {
+        return Type::all();
+    }
+
+    public function show($id)
+    {
+        return Type::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'file_name' => 'required|string',
+            ]);
+    
+            return Type::create($validatedData);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $model = Type::findOrFail($id);
+    
+            $validatedData = $request->validate([
+                'file_name' => 'required|string',
+            ]);
+    
+            $model->update($validatedData);
+    
+            return $model;
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 404);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $model = Type::findOrFail($id);
+            $model->delete();
+    
+            return response()->json(['message' => 'Record archived successfully'], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Record not found'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
